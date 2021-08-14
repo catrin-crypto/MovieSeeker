480f6ebe7d7311d01a3783c9bf6010ef3745b4b7
catrin-crypto <78826785+catrin-crypto@users.noreply.github.com>
2021-08-14 18:09:32 +0300

    lesson 3

--- app/src/main/java/com/example/movieseeker/framework/ui/FavouritesFragment.kt
+++ app/src/main/java/com/example/movieseeker/framework/ui/FavouritesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.movieseeker.framework.ui.main
+package com.example.movieseeker.framework.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.movieseeker.databinding.FavouritesFragmentBinding
+import com.example.movieseeker.framework.ui.main.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavouritesFragment : Fragment() {
